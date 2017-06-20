67a0f042db780f83a9af92ae3399886898ac4a58
katharinebeaumont <katharinehbeaumont@gmail.com>
2017-06-20 11:26:33 +0100

    Removing working code

--- src/cluster/steps/ClusterAssignmentStep.java
+++ src/cluster/steps/ClusterAssignmentStep.java
@@ -23,32 +23,7 @@ public class ClusterAssignmentStep {
         //Use EuclideanDistance to work out the distances between a data point, and the centroids.
         //You need to assign a data point to the index of the centroid it is closest to.
         //Think: what if a data point is equidistant to n centroids?
-        int number_centroids = centroids.size();
         
-        data_points.forEach((dp) -> {
-            int closest_centroid = 0;
-            if (number_centroids < 1) {
-                closest_centroid = 0;
-            } else {
-                double shortest_distance = 100;
-                Set<Integer> centroid_keys = centroids.keySet();
-                for (Integer centroid_key: centroid_keys) {
-                    double distance = EuclideanDistance.calculate(dp, centroids.get(centroid_key));
-                    if (distance < shortest_distance) {
-                        shortest_distance = distance;
-                        closest_centroid = centroid_key;
-                    }
-                }
-            }
-            
-            ArrayList<Coordinate> data_for_centroid = cluster_assignment.get(closest_centroid);
-            if (data_for_centroid == null) {
-                data_for_centroid = new ArrayList();
-            }
-            data_for_centroid.add(dp);
-            cluster_assignment.put(closest_centroid, data_for_centroid);
-            
-        });
         
         return cluster_assignment;
     }
--- src/cluster/steps/MoveCentroidStep.java
+++ src/cluster/steps/MoveCentroidStep.java
@@ -26,29 +26,7 @@ public class MoveCentroidStep {
         // and the average of all the y coordinates.
         // This will form the new location for the centroid.
         // Things to consider: if a centroid has no data points, it should be removed.
-        int centroids_num = centroid_index_to_data_points.size();
-        Set<Integer> centroid_indexes = centroid_index_to_data_points.keySet();
-        for (Integer centroid_index: centroid_indexes) {
-            double x_sum = 0;
-            double y_sum = 0;
-            int num_data_points = 0;
-            ArrayList<Coordinate> data_for_centroids = centroid_index_to_data_points.get(centroid_index);
-            if (data_for_centroids == null || data_for_centroids.isEmpty()) {
-                System.out.println("***Removing centroid " + centroid_indexes);
-            } else {
-                for (Coordinate data_point: data_for_centroids) {
-                    x_sum = x_sum + data_point.getX();
-                    y_sum = y_sum + data_point.getY();
-                    num_data_points++;
-                }
-
-                double new_x = x_sum/num_data_points;
-                double new_y = y_sum/num_data_points;
-
-                Coordinate new_centroid = new Coordinate(new_x, new_y);
-                new_centroids.put(centroid_index, new_centroid);
-            }
-        }
+        
         return new_centroids;
     }
     
