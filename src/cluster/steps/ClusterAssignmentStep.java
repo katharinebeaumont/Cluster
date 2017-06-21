e24fd60008eaa5e41f7d3bc2e08a7d13e98b86c9
katharinebeaumont <katharinehbeaumont@gmail.com>
2017-06-21 14:33:19 +0100

    Adding more helpful comments

--- src/cluster/steps/ClusterAssignmentStep.java
+++ src/cluster/steps/ClusterAssignmentStep.java
@@ -15,8 +15,14 @@ import java.util.HashMap;
  */
 public class ClusterAssignmentStep {
     
+    //ArrayList<Coordinate> data_points - this is the location of all of the data points
+    //HashMap<Integer, Coordinate> centroids - this is the index of the centroid, and it's location
+    // e.g. 1, (2,3) - centroid 1, is at location 2,3
+    //   2, (4,3) - centroid 2, is at location 4,3
     public static HashMap<Integer, ArrayList<Coordinate>> assignCluster(ArrayList<Coordinate> data_points, HashMap<Integer, Coordinate> centroids) {
         //This is what you need to return.
+        // This is - 
+        // index of the centroid, and a list of the data points that are closest to it
         HashMap<Integer, ArrayList<Coordinate>> cluster_assignment = new HashMap();
         
         //Use EuclideanDistance to work out the distances between a data point, and the centroids.
